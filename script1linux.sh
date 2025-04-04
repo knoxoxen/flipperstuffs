c3a2b1cc7e1a81c758d51cb316d175a886f6db85
knoxoxen <149332086+knoxoxen@users.noreply.github.com>
2025-04-04 11:51:42 -0400

    Add files via upload

---
+++ script1linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# My first script
+echo Downloading zip!
+wget https://ia800208.us.archive.org/29/items/Open_Sonic/OpenSonicWin.zip
+unzip OpenSonicWin.zip -d ~/Desktop/opensonic
+cd opensonic	
+wine opensonic.exe
