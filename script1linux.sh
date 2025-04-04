d54ac7b0ecc8a4fc1d59f887ee929c4f5e65df95
knoxoxen <149332086+knoxoxen@users.noreply.github.com>
2025-04-04 12:11:12 -0400

    Update script1linux.sh

--- script1linux.sh
+++ script1linux.sh
@@ -3,5 +3,5 @@
 echo Downloading zip!
 wget https://ia800208.us.archive.org/29/items/Open_Sonic/OpenSonicWin.zip
 unzip OpenSonicWin.zip -d ~/Desktop/opensonic
-cd ~/desktop/opensonic	
+cd ~/Desktop/opensonic	
 wine opensonic.exe
